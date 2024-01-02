01c9ad7f6a65de8e5ff0a40b6ba61b40f26fe54b
Ilya Orazov <ilordash@mail.ru>
2024-01-03 02:48:12 +0300

    format code

--- src/WebSerial.h
+++ src/WebSerial.h
@@ -6,15 +6,15 @@
 #include <functional>
 
 #if defined(ESP8266)
-    #define HARDWARE "ESP8266"
-    #include "ESP8266WiFi.h"
-    #include "ESPAsyncTCP.h"
-    #include "ESPAsyncWebServer.h"
+#define HARDWARE "ESP8266"
+#include "ESP8266WiFi.h"
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebServer.h"
 #elif defined(ESP32)
-    #define HARDWARE "ESP32"
-    #include "WiFi.h"
-    #include "AsyncTCP.h"
-    #include "ESPAsyncWebServer.h"
+#define HARDWARE "ESP32"
+#include "WiFi.h"
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
 #endif
 
 #include "webserial_webpage.h"
@@ -26,24 +26,24 @@ typedef std::function<void(uint8_t *data, size_t len)> RecvMsgHandler;
 
 class WebSerialClass : public Print {
 
-public:
-    void begin(AsyncWebServer *server, const char* url = "/webserial");
+  public:
+	void begin(AsyncWebServer *server, const char *url = "/webserial");
 
-    void msgCallback(RecvMsgHandler _recv);
+	void msgCallback(RecvMsgHandler _recv);
 
-    // Print
+	// Print
 
-    size_t write(uint8_t);
-    size_t write(const uint8_t* buffer, size_t size);
+	size_t write(uint8_t);
+	size_t write(const uint8_t *buffer, size_t size);
 
-private:
-    AsyncWebServer *_server;
-    AsyncWebSocket *_ws;
-    RecvMsgHandler _RecvFunc = NULL;
-    
-    #if defined(WEBSERIAL_DEBUG)
-        void DEBUG_WEB_SERIAL(const char* message);
-    #endif
+  private:
+	AsyncWebServer *_server;
+	AsyncWebSocket *_ws;
+	RecvMsgHandler _RecvFunc = NULL;
+
+#if defined(WEBSERIAL_DEBUG)
+	void DEBUG_WEB_SERIAL(const char *message);
+#endif
 };
 
 extern WebSerialClass WebSerial;
